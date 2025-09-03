771ba30599b2b974f1a0b21df3dc784fcb0150a0
RaviTwari03 <ravirkt0@gmail.com>
2025-09-03 08:45:53 +0530

    Initial Commit

---
+++ ChatAI/ChatAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAIApp.swift
+//  ChatAI
+//
+//  Created by Ravi Tiwari on 03/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
